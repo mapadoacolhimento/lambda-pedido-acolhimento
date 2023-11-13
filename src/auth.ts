d1ef72d124c4a695ca1e1ba19a7d6de7db54862d
Viviane Dias <vivipirolad@gmail.com>
2023-11-12 22:33:55 -0300

    feat: adds auth layer to endpoints

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,87 @@
-export default function auth() {
-  return "Hello from auth!";
-}
+import { verify } from "jsonwebtoken";
+import type {
+  Handler,
+  Context,
+  APIGatewayTokenAuthorizerEvent,
+  APIGatewayAuthorizerCallback,
+  APIGatewayAuthorizerResult,
+  Statement,
+} from "aws-lambda";
+
+const denyPolicy = function (
+  principalId: string,
+  resource: string,
+): APIGatewayAuthorizerResult {
+  return generatePolicy("Deny", principalId, resource);
+};
+
+const allowPolicy = function (
+  principalId: string,
+  resource: string,
+): APIGatewayAuthorizerResult {
+  return generatePolicy("Allow", principalId, resource);
+};
+
+const generatePolicy = function (
+  effect: string,
+  principalId: string,
+  resource: string,
+): APIGatewayAuthorizerResult {
+  let authResponse: APIGatewayAuthorizerResult = {
+    principalId,
+    policyDocument: {
+      Version: "2012-10-17", // default version
+      Statement: [],
+    },
+  };
+
+  if (effect && resource) {
+    const statementOne: Statement = {
+      Action: "execute-api:Invoke", // default action
+      Effect: effect,
+      Resource: resource,
+    };
+    const policyDocument = {
+      Statement: [statementOne],
+    };
+
+    authResponse = {
+      ...authResponse,
+      policyDocument: {
+        ...authResponse.policyDocument,
+        ...policyDocument,
+      },
+    };
+  }
+
+  return authResponse;
+};
+
+const auth: Handler = (
+  event: APIGatewayTokenAuthorizerEvent,
+  _context: Context,
+  callback: APIGatewayAuthorizerCallback,
+) => {
+  console.log("Received event", JSON.stringify(event, null, 2));
+
+  // remove the 'Bearer ' prefix from the auth token
+  const token = event.authorizationToken.replace(/Bearer /g, "");
+
+  if (!token) return callback(null, denyPolicy("anonymous", event.methodArn));
+
+  const secret = process.env["JWT_SECRET"] || "";
+
+  // verifies secret and checks exp
+  verify(token, secret, (err, verified) => {
+    if (err) {
+      console.error("JWT Error", err, err?.stack);
+      return callback(null, denyPolicy("anonymous", event.methodArn));
+    }
+    return callback(
+      null,
+      allowPolicy(verified?.sub?.toString() || "", event.methodArn),
+    );
+  });
+};
+
+export default auth;
