6547408abd0e4a20c584d9a58e17695f6e474e51
Viviane Dias <vivipirolad@gmail.com>
2023-11-15 17:40:30 -0300

    feat: only add new changes, since app is using different schema

--- prisma/migrations/20231115201746_init/migration.sql
+++ prisma/migrations/20231115201746_init/migration.sql
@@ -1,15 +1,15 @@
 -- CreateEnum
-CREATE TYPE "SupportType" AS ENUM ('PSYCOLOGICAL', 'LEGAL');
+CREATE TYPE "match"."support_type" AS ENUM ('PSYCOLOGICAL', 'LEGAL');
 
 -- CreateEnum
-CREATE TYPE "SupportRequestsStatus" AS ENUM ('OPEN', 'MATCHED', 'SOCIAL_ASSISTANT', 'PUBLIC_SERVICE', 'FINISHED');
+CREATE TYPE "match"."support_requests_status" AS ENUM ('OPEN', 'MATCHED', 'SOCIAL_ASSISTANT', 'PUBLIC_SERVICE', 'FINISHED');
 
 -- CreateTable
-CREATE TABLE "SupportRequests" (
+CREATE TABLE "match"."support_requests" (
     "support_request_id" SERIAL NOT NULL,
     "msr_id" INTEGER NOT NULL,
     "zendesk_ticket_id" INTEGER NOT NULL,
-    "support_type" "SupportType" NOT NULL,
+    "support_type" "match"."support_type" NOT NULL,
     "support_expertise" TEXT NOT NULL,
     "priority" INTEGER NOT NULL,
     "has_disability" BOOLEAN NOT NULL,
@@ -19,12 +19,12 @@ CREATE TABLE "SupportRequests" (
     "lng" DOUBLE PRECISION NOT NULL,
     "city" TEXT NOT NULL,
     "state" TEXT NOT NULL,
-    "status" "SupportRequestsStatus" NOT NULL,
+    "status" "match"."support_requests_status" NOT NULL,
     "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "SupportRequests_pkey" PRIMARY KEY ("support_request_id")
+    CONSTRAINT "support_requests_pkey" PRIMARY KEY ("support_request_id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "SupportRequests_zendesk_ticket_id_key" ON "SupportRequests"("zendesk_ticket_id");
+CREATE UNIQUE INDEX "support_requests_zendesk_ticket_id_key" ON "match"."support_requests"("zendesk_ticket_id");
