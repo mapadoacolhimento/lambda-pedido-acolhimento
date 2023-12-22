fd1fc43bf536f1e9a9c2e32136812c6af7fea353
Viviane Dias <vivianedias@protonmail.com>
2023-12-22 19:11:33 -0300

    feat: uses agent dicio to get agent options on getAgent func

--- src/utils/getAgent.ts
+++ src/utils/getAgent.ts
@@ -1,6 +1,9 @@
-export default function getAgent() {
-  const agentOptions = [377511446392, 377577169651];
+import { AGENT_DICIO } from "../constants";
+
+export default function getAgent(): number {
+  const agentOptions = Object.keys(AGENT_DICIO);
   const randomNum = Math.floor(Math.random() * 100);
   const agentIndex = randomNum % 2;
-  return agentOptions[agentIndex];
+  const agent = agentOptions[agentIndex];
+  return Number(agent);
 }
