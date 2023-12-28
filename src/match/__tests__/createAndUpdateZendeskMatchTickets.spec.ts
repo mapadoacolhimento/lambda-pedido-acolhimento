19ae12bfea705df72ea10a3378b2bb81e1ff46da
Viviane Dias <vivianedias@protonmail.com>
2023-12-28 11:33:25 -0300

    test: adds integration tests for createAndUpdateZendeskMatchTickets

--- src/match/__tests__/createAndUpdateZendeskMatchTickets.spec.ts
+++ src/match/__tests__/createAndUpdateZendeskMatchTickets.spec.ts
@@ -4,16 +4,23 @@ import type { SupportRequest, ZendeskTicket, ZendeskUser } from "../../types";
 
 import * as zendeskClient from "../../zendeskClient";
 import * as getAgent from "../../utils/getAgent";
+import * as getCurrentDate from "../../utils/getCurrentDate";
+import * as getMsrEmail from "../getMsrEmail";
 import { prismaMock } from "../../setupTests";
 
 const getAgentMock = jest.spyOn(getAgent, "default");
+const getCurrentDateMock = jest.spyOn(getCurrentDate, "default");
 const createTicketMock = jest.spyOn(zendeskClient, "createTicket");
-// const updateTicketMock = jest.spyOn(zendeskClient, "updateTicket");
+const updateTicketMock = jest.spyOn(zendeskClient, "updateTicket");
 const getUserMock = jest.spyOn(zendeskClient, "getUser");
+const getMsrEmailMock = jest.spyOn(getMsrEmail, "default");
 
 const mockAgentNumber = 1;
-const mockVolunteerTicket = {
+const mockVolunteerZendeskTicket = {
   id: 123123123 as unknown as bigint,
+} as ZendeskTicket;
+const mockMsrZendeskTicket = {
+  id: 123412341234 as unknown as bigint,
 } as ZendeskTicket;
 const baseSupportRequestPayload = {
   msrId: 123 as unknown as bigint,
@@ -29,9 +36,13 @@ const mockMsrFromZendesk = {
   id: 5 as unknown as bigint,
   name: "Teste MSR",
 } as ZendeskUser;
+const mockCurrentDate = "2023-12-28";
+const mockMsrEmail = "msr email";
 
 describe("createAndUpdateZendeskMatchTickets", () => {
   getAgentMock.mockImplementation(() => mockAgentNumber);
+  getCurrentDateMock.mockImplementation(() => mockCurrentDate);
+  getMsrEmailMock.mockImplementation(() => mockMsrEmail);
 
   it("should throw an error if no volunteer is found", async () => {
     getUserMock.mockResolvedValueOnce({} as ZendeskUser);
@@ -71,15 +82,103 @@ describe("createAndUpdateZendeskMatchTickets", () => {
   });
 
   describe("Legal", () => {
+    const legalSupportRequest = {
+      ...baseSupportRequestPayload,
+      supportType: "legal" as const,
+    };
+
     beforeEach(() => {
       getUserMock.mockResolvedValueOnce(mockMsrFromZendesk);
       prismaMock.volunteers.findUnique.mockResolvedValueOnce(
         mockVolunteerFromDB
       );
-      createTicketMock.mockResolvedValueOnce(mockVolunteerTicket);
+      createTicketMock.mockResolvedValueOnce(mockVolunteerZendeskTicket);
+      updateTicketMock.mockResolvedValueOnce(mockMsrZendeskTicket);
     });
-    it("should create a volunteer ticket with correct params", () => {});
-    it("should update msr ticket with correct params", () => {});
+
+    it("should return correct volunteer zendesk ticket id", async () => {
+      const volunteerZendeskTicketId = await createAndUpdateZendeskMatchTickets(
+        legalSupportRequest,
+        mockVolunteerId
+      );
+      expect(volunteerZendeskTicketId).toStrictEqual(123123123);
+    });
+
+    it("should create a volunteer ticket with correct params", async () => {
+      const volunteerTicket = {
+        external_id: 5,
+        requester_id: 3,
+        submitter_id: 1,
+        assignee_id: 1,
+        status: "pending",
+        subject: "[Advogada] Teste Voluntária",
+        organization_id: 360269610652,
+        comment: {
+          body: "Voluntária recebeu um pedido de acolhimento de Teste MSR",
+          author_id: 1,
+          public: false,
+        },
+        custom_fields: [
+          {
+            id: 360016681971,
+            value: "Teste MSR",
+          },
+          {
+            id: 360016631632,
+            value: "https://meudominio.zendesk.com/agent/tickets/1234",
+          },
+          {
+            id: 360014379412,
+            value: "encaminhamento__realizado",
+          },
+          {
+            id: 360017432652,
+            value: "2023-12-28",
+          },
+        ],
+      };
+      await createAndUpdateZendeskMatchTickets(
+        legalSupportRequest,
+        mockVolunteerId
+      );
+      expect(createTicketMock).toHaveBeenNthCalledWith(1, volunteerTicket);
+    });
+
+    it("should update msr ticket with correct params", async () => {
+      const msrTicket = {
+        id: 1234,
+        status: "pending",
+        assignee_id: 1,
+        custom_fields: [
+          {
+            id: 360016631592,
+            value: "Teste Voluntária",
+          },
+          {
+            id: 360014379412,
+            value: "encaminhamento__realizado",
+          },
+          {
+            id: 360016631632,
+            value: "https://meudominio.zendesk.com/agent/tickets/123123123",
+          },
+          {
+            id: 360017432652,
+            value: "2023-12-28",
+          },
+        ],
+        comment: {
+          body: "msr email",
+          author_id: 1,
+          public: true,
+        },
+      };
+      await createAndUpdateZendeskMatchTickets(
+        legalSupportRequest,
+        mockVolunteerId
+      );
+      expect(updateTicketMock).toHaveBeenNthCalledWith(1, msrTicket);
+    });
   });
 
   describe("Psychological", () => {
@@ -88,9 +187,67 @@ describe("createAndUpdateZendeskMatchTickets", () => {
       prismaMock.volunteers.findUnique.mockResolvedValueOnce(
         mockVolunteerFromDB
       );
-      createTicketMock.mockResolvedValueOnce(mockVolunteerTicket);
+      createTicketMock.mockResolvedValueOnce(mockVolunteerZendeskTicket);
+      updateTicketMock.mockResolvedValueOnce(mockMsrZendeskTicket);
     });
-    it("should create volunteer ticket with correct params", () => {});
-    it("should update msr ticket with correct params", () => {});
+
+    it("should return correct volunteer zendesk ticket id", async () => {
+      const volunteerZendeskTicketId = await createAndUpdateZendeskMatchTickets(
+        baseSupportRequestPayload,
+        mockVolunteerId
+      );
+      expect(volunteerZendeskTicketId).toStrictEqual(123123123);
+    });
+
+    it("should create volunteer ticket with correct params", async () => {
+      const volunteerTicket = {
+        subject: "[Psicóloga] Teste Voluntária",
+        organization_id: 360282119532,
+      };
+      await createAndUpdateZendeskMatchTickets(
+        baseSupportRequestPayload,
+        mockVolunteerId
+      );
+      expect(createTicketMock).toHaveBeenNthCalledWith(
+        1,
+        expect.objectContaining(volunteerTicket)
+      );
+    });
+
+    it("should update msr ticket with correct params", async () => {
+      const msrTicket = {
+        id: 1234,
+        status: "pending",
+        assignee_id: 1,
+        custom_fields: [
+          {
+            id: 360016631592,
+            value: "Teste Voluntária",
+          },
+          {
+            id: 360014379412,
+            value: "encaminhamento__realizado",
+          },
+          {
+            id: 360016631632,
+            value: "https://meudominio.zendesk.com/agent/tickets/123123123",
+          },
+          {
+            id: 360017432652,
+            value: "2023-12-28",
+          },
+        ],
+        comment: {
+          body: "msr email",
+          author_id: 1,
+          public: true,
+        },
+      };
+      await createAndUpdateZendeskMatchTickets(
+        baseSupportRequestPayload,
+        mockVolunteerId
+      );
+      expect(updateTicketMock).toHaveBeenNthCalledWith(1, msrTicket);
+    });
   });
 });
