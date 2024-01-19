c3d59dde98fd5d1f3b46beb49108090fa457667a
Viviane Dias <vivianedias@protonmail.com>
2024-01-19 16:17:33 -0300

    chore: adds `/token` to zendesk api user env var export

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const ZENDESK_SUBDOMAIN = process.env["ZENDESK_SUBDOMAIN"];
 export const ZENDESK_API_URL = `${ZENDESK_SUBDOMAIN}/api/v2`;
-export const ZENDESK_API_USER = process.env["ZENDESK_API_USER"];
+export const ZENDESK_API_USER = `${process.env["ZENDESK_API_USER"]}/token`;
 export const ZENDESK_API_TOKEN = process.env["ZENDESK_API_TOKEN"];
 
 export const AGENT_DICIO: Record<number, string> = {
