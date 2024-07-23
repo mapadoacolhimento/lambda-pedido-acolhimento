3160ad97b9149400f9dafc4c137ac1c458c65245
Camila Cardoso <camilacardoso@Mapa-MacBook-Air.local>
2024-07-23 15:49:41 -0300

    feat: creates create-match endpoint

---
+++ src/createMatch.ts
@@ -0,0 +1,74 @@
+import type {
+  APIGatewayEvent,
+  Context,
+  APIGatewayProxyCallback,
+} from "aws-lambda";
+import { object, number } from "yup";
+
+import { getErrorMessage, isJsonString, stringfyBigInt } from "./utils";
+
+const bodySchema = object({
+  supportRequestId: number().required(),
+  volunteerId: number().required(),
+})
+  .required()
+  .strict();
+
+export default async function handler(
+  event: APIGatewayEvent,
+  _context: Context,
+  callback: APIGatewayProxyCallback
+) {
+  try {
+    const body = event.body;
+
+    if (!body) {
+      const errorMessage = "Empty request body";
+      console.error(`[manual-match] - [400]: ${errorMessage}`);
+
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: errorMessage,
+        }),
+      });
+    }
+
+    const parsedBody = isJsonString(body)
+      ? (JSON.parse(body) as unknown)
+      : (Object.create(null) as Record<string, unknown>);
+
+    const validatedBody = await bodySchema.validate(parsedBody);
+
+    const bodyRes = JSON.stringify({
+      message: stringfyBigInt(validatedBody),
+    });
+
+    return callback(null, {
+      statusCode: 200,
+      body: bodyRes,
+    });
+  } catch (e) {
+    const error = e as Record<string, unknown>;
+    if (error["name"] === "ValidationError") {
+      const errorMsg = `Validation error: ${getErrorMessage(error)}`;
+
+      console.error(`[manual-match] - [400]: ${errorMsg}`);
+
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: errorMsg,
+        }),
+      });
+    }
+
+    const errorMsg = getErrorMessage(error);
+    console.error(`[manual-match] - [500]: ${errorMsg}`);
+
+    return callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({ error: errorMsg }),
+    });
+  }
+}
