7e81a780de3e42d5795c2e67991bff8a66ab3019
Camila Cardoso <camilacardoso@Mapa-MacBook-Air.local>
2024-07-23 16:11:57 -0300

    feat: adds logic for creating match

--- src/createMatch.ts
+++ src/createMatch.ts
@@ -3,13 +3,19 @@ import type {
   Context,
   APIGatewayProxyCallback,
 } from "aws-lambda";
-import { object, number } from "yup";
+import { object, number, string } from "yup";
+
+import client from "./prismaClient";
 
 import { getErrorMessage, isJsonString, stringfyBigInt } from "./utils";
+import { createMatch } from "./match/createMatch";
+import { MatchStage, MatchType } from "@prisma/client";
 
 const bodySchema = object({
   supportRequestId: number().required(),
   volunteerId: number().required(),
+  matchType: string().oneOf(Object.values(MatchType)).required(),
+  matchStage: string().oneOf(Object.values(MatchStage)).required(),
 })
   .required()
   .strict();
@@ -24,7 +30,7 @@ export default async function handler(
 
     if (!body) {
       const errorMessage = "Empty request body";
-      console.error(`[manual-match] - [400]: ${errorMessage}`);
+      console.error(`[create-match] - [400]: ${errorMessage}`);
 
       return callback(null, {
         statusCode: 400,
@@ -40,8 +46,40 @@ export default async function handler(
 
     const validatedBody = await bodySchema.validate(parsedBody);
 
+    const { supportRequestId, volunteerId, matchType, matchStage } =
+      validatedBody;
+
+    const supportRequest = await client.supportRequests.findUnique({
+      where: { supportRequestId: supportRequestId },
+    });
+
+    if (!supportRequest) {
+      const errorMessage = `support_request not found for support_request_id '${supportRequestId}'`;
+
+      return callback(null, notFoundErrorPayload(errorMessage));
+    }
+
+    const volunteerAvailability = await client.volunteerAvailability.findUnique(
+      {
+        where: { volunteer_id: volunteerId },
+      }
+    );
+
+    if (!volunteerAvailability) {
+      const errorMessage = `volunteer_availability not found for volunteer_id '${volunteerId}'`;
+
+      return callback(null, notFoundErrorPayload(errorMessage));
+    }
+
+    const match = await createMatch(
+      supportRequest,
+      volunteerAvailability,
+      matchType,
+      matchStage
+    );
+
     const bodyRes = JSON.stringify({
-      message: stringfyBigInt(validatedBody),
+      message: stringfyBigInt(match),
     });
 
     return callback(null, {
@@ -53,7 +91,7 @@ export default async function handler(
     if (error["name"] === "ValidationError") {
       const errorMsg = `Validation error: ${getErrorMessage(error)}`;
 
-      console.error(`[manual-match] - [400]: ${errorMsg}`);
+      console.error(`[create-match] - [400]: ${errorMsg}`);
 
       return callback(null, {
         statusCode: 400,
@@ -64,11 +102,22 @@ export default async function handler(
     }
 
     const errorMsg = getErrorMessage(error);
-    console.error(`[manual-match] - [500]: ${errorMsg}`);
+    console.error(`[create-match] - [500]: ${errorMsg}`);
 
     return callback(null, {
       statusCode: 500,
       body: JSON.stringify({ error: errorMsg }),
     });
   }
+}
+
+function notFoundErrorPayload(errorMessage: string) {
+  console.error(`[create-match] - [404]: ${errorMessage}`);
+
+  return {
+    statusCode: 404,
+    body: JSON.stringify({
+      error: errorMessage,
+    }),
+  };
 }
