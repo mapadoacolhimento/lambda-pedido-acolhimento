d072442a8c50a2b5032c9303487019b2f3dcf51f
Viviane Dias <vivi@mapa.org.br>
2024-07-31 17:14:33 -0300

    feat: separates volunteer logic from matchLogic

---
+++ src/match/volunteer.ts
@@ -0,0 +1,65 @@
+import type { VolunteerAvailability } from "@prisma/client";
+import type { SupportRequestGeoreference } from "../types";
+import {
+  filterVolunteersInTheSameState,
+  findClosestVolunteer,
+  findVolunteerInTheSameCity,
+} from "./matchLogic";
+import { IDEAL_MATCH_MAX_DISTANCE } from "../constants";
+
+export function getIdealVolunteer(
+  supportRequestGeoreference: SupportRequestGeoreference,
+  availableVolunteers: VolunteerAvailability[]
+): VolunteerAvailability | null {
+  return findClosestVolunteer(
+    supportRequestGeoreference.lat,
+    supportRequestGeoreference.lng,
+    availableVolunteers,
+    IDEAL_MATCH_MAX_DISTANCE
+  );
+}
+
+export function getExpandedVolunteer(
+  supportRequestGeoreference: SupportRequestGeoreference,
+  availableVolunteers: VolunteerAvailability[]
+): VolunteerAvailability | null {
+  return findVolunteerInTheSameCity(
+    supportRequestGeoreference.city,
+    supportRequestGeoreference.state,
+    availableVolunteers
+  );
+}
+
+export default function getOnlineVolunteer(
+  supportRequestGeoreference: SupportRequestGeoreference,
+  availableVolunteers: VolunteerAvailability[]
+): VolunteerAvailability | null {
+  if (availableVolunteers.length === 0) return null;
+
+  const volunteersInTheSameState = filterVolunteersInTheSameState(
+    supportRequestGeoreference.state,
+    availableVolunteers
+  );
+
+  if (volunteersInTheSameState.length > 0) {
+    const closestVolunteerInTheSameState = findClosestVolunteer(
+      supportRequestGeoreference.lat,
+      supportRequestGeoreference.lng,
+      volunteersInTheSameState,
+      null
+    );
+
+    if (closestVolunteerInTheSameState) return closestVolunteerInTheSameState;
+  }
+
+  const closestVolunteer = findClosestVolunteer(
+    supportRequestGeoreference.lat,
+    supportRequestGeoreference.lng,
+    availableVolunteers,
+    null
+  );
+
+  if (closestVolunteer) return closestVolunteer;
+
+  return availableVolunteers[0] || null;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./Zendesk";
 export * from "./SupportRequest";
+export * from "./Volunteer";
