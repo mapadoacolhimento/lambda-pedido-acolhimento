88183e7918513c3b8481500303cecffae8470136
dieh <dri.leela@gmail.com>
2026-01-08 14:38:18 -0300

    feat: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,24 @@
+// webpack.config.js - VERSÃO FUNCIONAL
 const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 const slsw = require("serverless-webpack");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 const isLocal = slsw.lib.webpack.isLocal;
-const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   mode: isLocal ? "development" : "production",
   entry: slsw.lib.entries,
 
-  // SOLUÇÃO: Configuração correta do cache para Webpack 5
-  cache: {
-    type: "filesystem",
-    cacheDirectory: path.resolve(__dirname, ".webpackCache"),
-    buildDependencies: {
-      config: [__filename],
-    },
-  },
-
-  externals: [
-    nodeExternals({
-      allowlist: [
-        /\.prisma/,
-        /@prisma/,
-        /@turf/,
-        /yup/,
-        /jsonwebtoken/,
-        /dotenv-safe/,
-      ],
+  ...(process.env.WEBPACK_CACHE !== "false" &&
+    isLocal && {
+      cache: {
+        type: "filesystem",
+        cacheDirectory: path.resolve(__dirname, ".webpackCache"),
+      },
     }),
-  ],
 
+  externals: [nodeExternals()],
   devtool: isLocal ? "source-map" : false,
 
   resolve: {
@@ -44,24 +29,6 @@ module.exports = {
     libraryTarget: "commonjs2",
     path: path.join(__dirname, ".webpack"),
     filename: "[name].js",
-  },
-
-  optimization: {
-    minimize: !isLocal,
-    minimizer: !isLocal
-      ? [
-          new TerserPlugin({
-            terserOptions: {
-              compress: {
-                drop_console: true,
-                drop_debugger: true,
-              },
-              mangle: true,
-            },
-            extractComments: false,
-          }),
-        ]
-      : [],
   },
 
   module: {
@@ -75,36 +42,16 @@ module.exports = {
   },
 
   plugins: [
-    ...(isLocal ? [new ForkTsCheckerWebpackPlugin()] : []),
-
     new CopyPlugin({
       patterns: [
         {
           from: "./node_modules/.prisma/client/schema.prisma",
-          to: "./prisma",
-        },
-        {
-          from: "./node_modules/.prisma/client/libquery_engine-*",
-          to: ({ absoluteFilename }) => {
-            const filename = path.basename(absoluteFilename);
-            if (
-              filename.includes("linux") ||
-              filename.includes("debian") ||
-              process.env.CI
-            ) {
-              return `./prisma/${filename}`;
-            }
-            return `./prisma/${filename}`;
-          },
+          to: "./prisma/schema.prisma",
         },
       ],
     }),
   ],
 
-  stats: {
-    warnings: false,
-    modules: false,
-    chunks: false,
-    colors: true,
-  },
+  // Ignora avisos
+  ignoreWarnings: [{ module: /node_modules/ }],
 };
